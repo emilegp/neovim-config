b598a2e02d79323be22a3c8389fed23b03d9e50c
Émile Guertin-Picard <emile.guertin-picard@polymtl.ca>
2024-07-29 22:05:52 -0400

    TLDR hello world of blog done

--- init.lua
+++ init.lua
@@ -4,3 +4,17 @@ require("config.keybindings")
 require("config.vim-options")
 require("config.tldraw")
 require("config.neovide")
+
+vim.cmd[[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+
+-- Load snippets from ~/.config/nvim/LuaSnip/
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -6,45 +6,19 @@ return {
     'tpope/vim-surround'
   },
   {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets"
-    },
-    config = function()
-      local ls = require("luasnip")
-      require("luasnip.loaders.from_vscode").lazy_load()
-      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/config/latex-snippets.lua" })
-
-      ls.config.set_config {
-        history = true,
-        updateevents = "TextChanged,TextChangedI",
-        enable_autosnippets = true,  -- Ensure autosnippets are enabled
-      }
-
-      -- Optional: Set up key mappings for snippet navigation
-      vim.api.nvim_set_keymap('i', '<C-k>', [[<Cmd>lua require'luasnip'.expand_or_jump()<CR>]], { silent = true })
-      vim.api.nvim_set_keymap('s', '<C-k>', [[<Cmd>lua require'luasnip'.expand_or_jump()<CR>]], { silent = true })
-      vim.api.nvim_set_keymap('i', '<C-j>', [[<Cmd>lua require'luasnip'.jump(-1)<CR>]], { silent = true })
-      vim.api.nvim_set_keymap('s', '<C-j>', [[<Cmd>lua require'luasnip'.jump(-1)<CR>]], { silent = true })
-      vim.api.nvim_set_keymap('i', '<C-l>', [[<Cmd>lua require'luasnip'.change_choice(1)<CR>]], { silent = true })
-      vim.api.nvim_set_keymap('s', '<C-l>', [[<Cmd>lua require'luasnip'.change_choice(1)<CR>]], { silent = true })
-    end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require('cmp')
-      local luasnip = require('luasnip')
+--      local luasnip = require('luasnip')
 
-      require("luasnip.loaders.from_vscode").lazy_load()
+--      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
+--        snippet = {
+--         expand = function(args)
+--            require('luasnip').lsp_expand(args.body)
+--          end,
+--        },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
@@ -58,8 +32,8 @@ return {
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
+--            elseif luasnip.expand_or_jumpable() then
+--              luasnip.expand_or_jump()
             else
               fallback()
             end
@@ -67,8 +41,8 @@ return {
           ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
+--            elseif luasnip.jumpable(-1) then
+--              luasnip.jump(-1)
             else
               fallback()
             end
@@ -76,7 +50,7 @@ return {
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
-          { name = 'luasnip' },
+--          { name = 'luasnip' },
         }, {
           { name = 'buffer' },
         }),
